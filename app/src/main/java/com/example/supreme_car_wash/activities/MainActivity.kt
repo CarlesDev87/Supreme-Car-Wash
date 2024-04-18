d68c4cfb4089f8929c3b516311ca995dfd6b85b4
carles <carlagmor@alu.edu.gva.es>
2024-04-18 08:35:12 +0200

    Primer commit

---
+++ app/src/main/java/com/example/supreme_car_wash/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.supreme_car_wash.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.supreme_car_wash.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
---
+++ app/src/main/java/com/example/supreme_car_wash/activities/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.supreme_car_wash.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
+import com.example.supreme_car_wash.R
+import com.example.supreme_car_wash.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val lottieAnimationView: LottieAnimationView = binding.animationView
+
+        lottieAnimationView.playAnimation()
+
+        val splashScreenDuration = 3000
+
+        binding.animationView.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashScreenDuration.toLong())
+
+    }
+}
