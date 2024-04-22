a5b7b4f229b5b7d30690b5932b0aac8b67ea574d
carles <carlagmor@alu.edu.gva.es>
2024-04-22 10:17:50 +0200

    Adapter terminado

--- app/src/main/java/com/example/supreme_car_wash/activities/MainActivity.kt
+++ app/src/main/java/com/example/supreme_car_wash/activities/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.supreme_car_wash.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.supreme_car_wash.R
+import com.example.supreme_car_wash.adapters.LavadoAdapter
 import com.example.supreme_car_wash.databinding.ActivityMainBinding
 import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var linearLayout: LinearLayoutManager
+    private lateinit var lavadoAdapter: LavadoAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/com/example/supreme_car_wash/adapters/LavadoAdapter.kt
+++ app/src/main/java/com/example/supreme_car_wash/adapters/LavadoAdapter.kt
@@ -1,20 +1,52 @@
 package com.example.supreme_car_wash.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.supreme_car_wash.R
+import com.example.supreme_car_wash.databinding.ItemCochesBinding
 import com.example.supreme_car_wash.responses.LavadoResponse
+import com.example.supreme_car_wash.responses.VehiculoResponse
 
-class LavadoAdapter (private val lavados: List<LavadoResponse>, private val listener: OnClickListenerLavado): RecyclerView.Adapter<LavadoAdapter.ViewHolder>() {
+class LavadoAdapter(
+    private val lavados: List<LavadoResponse>, private val vehiculos: List<VehiculoResponse>,
+    private val listener: OnClickListenerLavado
+) : RecyclerView.Adapter<LavadoAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LavadoAdapter.ViewHolder {
-
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_coches, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: LavadoAdapter.ViewHolder, position: Int) {
+        val lavado = lavados[position]
+        val vehiculo = vehiculos[position]
+        with(holder) {
+            setListener(lavado)
+            binding.tipoLavado.text = "Tipo Lavado: ${lavado.tipoLavado}"
+            binding.marcaCoche.text = "Marca: ${vehiculo.marca}"
+            binding.modeloCoche.text = "Modelo: ${vehiculo.modelo}"
+            binding.matriculaCoche.text = "Matricula: ${vehiculo.matricula}"
+            binding.precioLavado.text = "Precio: ${lavado.precio}"
+            binding.cantidad.text = lavado.precio.toString()
+        }
 
     }
 
-    override fun getItemCount(): Int {
+    override fun getItemCount(): Int = lavados.size
 
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemCochesBinding.bind(view)
+
+        fun setListener(lavado: LavadoResponse) {
+            binding.root.setOnClickListener {
+                listener.onClick(lavado)
+            }
+        }
     }
 
 
